18e3325beb97f2a1c69d7e8ec342dde43081d8d5
Swathi Mukkamala <swathikmukkamala@gmail.com>
2020-08-11 14:47:37 -0400

    building API controllers, route and query parameters

---
+++ CoffeeShop/Controllers/CoffeeController.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using CoffeeShop.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace CoffeeShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoffeesController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public CoffeesController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string BeanTypeParam, int Limit, string sort)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    string query = "SELECT Id, Title, BeanType FROM Coffee ";
+
+                    if (Limit != 0)
+                    {
+                        query = $"SELECT TOP {Limit} Id, Title, BeanType FROM Coffee ";
+                    }
+
+                    if(BeanTypeParam != null)
+                    {
+                        query += $"WHERE BeanType = '{BeanTypeParam}'";
+                    }
+
+                    if(sort == "title")
+                    {
+                        query += "ORDER BY Title asc";
+                    }else if (sort == "beanType")
+                    {
+                        query += "ORDER BY BeanType asc";
+                    }
+
+                    cmd.CommandText = query;
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<Coffee> coffees = new List<Coffee>();
+
+                    while (reader.Read())
+                    {
+                        Coffee coffee = new Coffee
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            BeanType = reader.GetString(reader.GetOrdinal("BeanType"))
+                        };
+
+                        coffees.Add(coffee);
+                    }
+                    reader.Close();
+
+                    return Ok(coffees);
+                }
+            }
+        }
+
+        [HttpGet("{id}", Name = "GetCoffee")]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT
+                            Id, Title, BeanType
+                        FROM Coffee
+                        WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Coffee coffee = null;
+
+                    if (reader.Read())
+                    {
+                        coffee = new Coffee
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            BeanType = reader.GetString(reader.GetOrdinal("BeanType"))
+                        };
+                    }
+                    reader.Close();
+
+                    return Ok(coffee);
+                }
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Coffee coffee)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Coffee (Title, BeanType)
+                                        OUTPUT INSERTED.Id
+                                        VALUES (@title, @beanType)";
+                    cmd.Parameters.Add(new SqlParameter("@title", coffee.Title));
+                    cmd.Parameters.Add(new SqlParameter("@beanType", coffee.BeanType));
+
+                    int newId = (int)cmd.ExecuteScalar();
+                    coffee.Id = newId;
+                    return CreatedAtRoute("GetCoffee", new { id = newId }, coffee);
+                }
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Coffee coffee)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE Coffee
+                                            SET Title = @title,
+                                                BeanType = @beanType
+                                            WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@title", coffee.Title));
+                        cmd.Parameters.Add(new SqlParameter("@beanType", coffee.BeanType));
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!CoffeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM Coffee WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!CoffeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private bool CoffeeExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, Title, BeanType
+                        FROM Coffee
+                        WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    return reader.Read();
+                }
+            }
+        }
+    }
+}
---
+++ CoffeeShop/Models/Coffee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Models
+{
+    public class Coffee
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 2)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 2)]
+        public string BeanType { get; set; }
+    }
+}
